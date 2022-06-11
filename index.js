90aefadd18ca0e4450e79c1b44268db3314e68c2
Jersabell <jersabell@gmail.com>
2022-06-11 01:39:14 -0500

    first changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -9,6 +10,21 @@ const { port, dbUrl, secret } = config;
 const app = express();
 
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
+const connection = mysql.createConnection({
+  host: dbUrl,
+  user: 'root',
+  password: 'supersecret',
+  database: 'bq',
+});
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', (error, results) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log(`The solution is: ${results[0].solution}`);
+});
+connection.end();
 
 app.set('config', config);
 app.set('pkg', pkg);
